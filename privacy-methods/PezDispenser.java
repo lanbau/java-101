d217287d185000c5c5a01f853310764f98710fd4
Irvin Lin <irvinln@gmail.com>
2016-02-27 16:11:09 +0800

    Create PezDispenser.java

---
+++ privacy-methods/PezDispenser.java
@@ -0,0 +1,9 @@
+public class PezDispenser {
+  // change to private, only access through method
+  private String mCharacterName = "yoda"; 
+  
+  // declare return type which is String in this case
+  public String getCharacterName() {
+    return mCharacterName;
+  }
+}
