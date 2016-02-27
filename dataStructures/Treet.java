c04900268da9bea3c0c5fef4cdb989f13fa4548d
Irvin Lin <irvinln@gmail.com>
2016-02-27 21:19:29 +0800

    Update Treet.java

--- dataStructures/Treet.java
+++ dataStructures/Treet.java
@@ -1,4 +1,30 @@
 package com.teamtreehouse;
 
+import java.util.Date;
+
 public class Treet {
+  //immutable variables
+  private String mAuthor;
+  private String mDescription;
+  private Date mCreationDate;
+  
+  //constructor
+  public Treet(String author, String description, Date creationDate) {
+    mAuthor = author;
+    mDescription = description;
+    mCreationDate = creationDate;
+  }
+  
+  //NOTE! all methods must have RETURN TYPE Declared!
+  public String getAuthor() {
+    return mAuthor;
+  }
+  
+  public String getDescription() {
+    return mDescription;
+  }
+  
+  public Date getCreationDate() {
+    return mCreationDate;
+  }
 }
