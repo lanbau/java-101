aec61e7ac4e5eb47a55ed53b9742b79f01fb18c3
Irvin Lin <irvinln@gmail.com>
2016-02-27 16:10:14 +0800

    Create Example.java

---
+++ privacy-methods/Example.java
@@ -0,0 +1,8 @@
+public class Example {
+    
+    public static void main(String[] args) {
+        PezDispenser dispenser = new PezDispenser();
+        // Call method
+        System.out.printf("pez dispenser is %s\n", dispenser.getCharacterName());
+    }
+}
