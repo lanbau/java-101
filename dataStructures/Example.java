483e383efd67f80cbc5d36f0e3f2649810a775c9
Irvin Lin <irvinln@gmail.com>
2016-02-27 21:18:31 +0800

    Update Example.java

--- dataStructures/Example.java
+++ dataStructures/Example.java
@@ -1,11 +1,18 @@
 //folder structure: com -> teamtreehouse -> Treet.java
 
+import java.util.Date;
 import com.teamtreehouse.Treet;
 
 public class Example {
   
   public static void main(String[] args) {
-    Treet treet = new Treet();
+    Treet treet = new Treet(
+      "Irvin Lin",
+      "I love Node.JS",
+      new Date(1456578869L)
+      //epoch converter
+    );
     System.out.printf("this is a new %s %n", treet);
+    System.out.print(treet.getCreationDate());
   }
 }
