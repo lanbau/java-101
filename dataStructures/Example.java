022b7866d0e7da93a6153839ffdfc02287661b19
Irvin Lin <irvinln@gmail.com>
2016-02-27 21:06:51 +0800

    Create Example.java

---
+++ dataStructures/Example.java
@@ -0,0 +1,11 @@
+//folder structure: com -> teamtreehouse -> Treet.java
+
+import com.teamtreehouse.Treet;
+
+public class Example {
+  
+  public static void main(String[] args) {
+    Treet treet = new Treet();
+    System.out.printf("this is a new %s %n", treet);
+  }
+}
