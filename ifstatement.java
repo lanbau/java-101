15c88832f3f05ae2932b66e9fc6078064e82faf6
Irvin Lin <irvinln@gmail.com>
2016-02-27 12:29:35 +0800

    Update ifstatement.java

--- ifstatement.java
+++ ifstatement.java
@@ -5,6 +5,21 @@ public class TreeStory {
     public static void main(String[] args) {
         Console console = System.console();
         
+         
+        // Get Integer From String
+        String weightAsString = console.readLine("Enter Ur Weight: ");
+        int weight = Integer.parseInt(weightAsString);
+        System.out.print(weight);
+      
+        // String Equality
+        String vulgarwords = console.readLine("If Someone Stepped on Your Feet, What should you say?");
+        if (vulgarwords.equals("FUCK YOU")) {
+          console.printf("FUCK YOU TOO!");
+        } else {
+          console.printf("You little bitch!");
+        }
+      
+        // Simple If Statement & Program Exit
         int age = 12;  
         System.out.print(age);
         
